2f7d7c185fb16be0d7813004e804f75bf039f6b6
Aleksandra095 <aleksandrafilatova095@gmail.com>
2023-05-21 01:18:22 +0300

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,21 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
+const galleryList = document.querySelector('.gallery')
+const markup = galleryItems.map(({ preview, original, description }) => {
+    return `
+        <a class="gallery__item" href="${original}">
+    <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+    />
+        </a>
+    `
+}).join('')
 
-console.log(galleryItems);
+galleryList.insertAdjacentHTML('afterbegin', markup);
+new SimpleLightbox('.gallery a', { overlayOpacity: 0.8, captionsData: 'alt', captionDelay: 250, showCounter: false });
+
