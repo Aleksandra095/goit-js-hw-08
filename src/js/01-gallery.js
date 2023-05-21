1b6f24a8d6858652a5b1c0be7f3511c261c42f46
Aleksandra095 <aleksandrafilatova095@gmail.com>
2023-05-21 13:45:23 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -7,11 +7,11 @@ const galleryList = document.querySelector('.gallery')
 const markup = galleryItems.map(({ preview, original, description }) => {
     return `
         <a class="gallery__item" href="${original}">
-    <img
-        class="gallery__image"
-        src="${preview}"
-        alt="${description}"
-    />
+        <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+        />
         </a>
     `
 }).join('')
