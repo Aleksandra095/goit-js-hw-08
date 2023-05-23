37413cb6f812b1df50d4ac90e7806bae512989b9
Aleksandra095 <aleksandrafilatova095@gmail.com>
2023-05-23 19:34:23 +0300

    2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,5 +1,5 @@
 import VimeoPlayer from '@vimeo/player';
-import throttle from 'lodash.throttle';
+const throttle = require('lodash.throttle');
 
 let savedData = localStorage.getItem('videoplayer-current-time')
 const parseData = JSON.parse(savedData)
@@ -12,7 +12,4 @@ player.on('timeupdate', throttle(onLocalRecordTime, 1000))
 function onLocalRecordTime(event) {
     localStorage.setItem('videoplayer-current-time', JSON.stringify(event))
 };
-
-if (parseData && parseData.seconds) {
-    player.setCurrentTime(parseData.seconds);
-}
+player.setCurrentTime(parseData ? parseData.seconds : 0)
