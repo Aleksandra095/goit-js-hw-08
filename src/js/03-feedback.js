5efe8d2516d38adadb2f201f34eaeff9726349ca
Aleksandra095 <aleksandrafilatova095@gmail.com>
2023-05-23 20:56:48 +0300

    3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
+
+const feedbackStateKey = 'feedback-form-state';
+
+form.addEventListener('input', throttle(saveFormState, 500));
+window.addEventListener('load', populateFormState);
+form.addEventListener('submit', submitForm);
+
+function saveFormState(){
+    const formData = {
+        email: emailInput.value,
+        message: messageInput.value
+    };
+    localStorage.setItem(feedbackStateKey, JSON.stringify(formData));
+}
+function populateFormState() {
+    const savedData = localStorage.getItem(feedbackStateKey);
+    if (savedData) {
+        const formData = JSON.parse(savedData);
+        emailInput.value = formData.email || '';
+        messageInput.value = formData.message || '';
+    }
+}
+function submitForm(event) {
+    event.preventDefault();
+    const formData = {
+        email: emailInput.value,
+        message: messageInput.value
+    };
+    localStorage.removeItem(feedbackStateKey);
+    emailInput.value = '';
+    messageInput.value = '';
+}
