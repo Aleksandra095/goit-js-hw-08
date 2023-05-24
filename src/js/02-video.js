577f99bcdf74913020bb9f6110be1ad64070d690
Aleksandra095 <aleksandrafilatova095@gmail.com>
2023-05-24 22:21:05 +0300

    fix2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,20 +1,27 @@
 import VimeoPlayer from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-const savedData = localStorage.getItem('videoplayer-current-time');
-const parseData = JSON.parse(savedData);
-
-const iframe = document.querySelector('iframe');
-const player = new VimeoPlayer(iframe);
-
-player.on('time', throttle(onLocalRecordTime, 1000));
-
-function onLocalRecordTime(event) {
-    localStorage.setItem('videoplayer-current-time', JSON.stringify(event.seconds));
-}
-
-if (parseData) {
-    player.setCurrentTime(parseData);
-} else {
-    player.setCurrentTime(0);
-}
+document.addEventListener('DOMContentLoaded', function () {
+    const iframe = document.querySelector('iframe');
+    const player = new VimeoPlayer(iframe);
+    let currentTime = 0;
+    player.on('timeupdate', throttle(onVideoTimeUpdate, 1000));
+    player.on('pause', onVideoPause);
+    player.on('play', onVideoPlay);
+function onVideoTimeUpdate(event) {
+    currentTime = event.seconds;
+    }
+function onVideoPause() {
+    console.log('Час відтворення відео при зупинці:', currentTime);
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(currentTime));
+    }
+function onVideoPlay() {
+    const savedData = localStorage.getItem('videoplayer-current-time');
+    const parseData = JSON.parse(savedData);
+        if (parseData) {
+        currentTime = parseData;
+        player.setCurrentTime(currentTime);
+        console.log('Час відтворення', currentTime);
+        }
+    }
+});
