0aae0203b32b44b667b57860cbcca60be136cc6f
Aleksandra095 <aleksandrafilatova095@gmail.com>
2023-05-25 22:15:12 +0300

    bugfix2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,28 +1,16 @@
-import VimeoPlayer from '@vimeo/player';
+import Vimeo from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-document.addEventListener('DOMContentLoaded', function () {
-    const iframe = document.querySelector('iframe');
-    const player = new VimeoPlayer(iframe);
-    let currentTime = 0;
-    
-    player.on('timeupdate', throttle(onVideoTimeUpdate, 1000));
-    player.on('pause', onVideoPause);
-    player.on('play', onVideoPlay);
-function onVideoTimeUpdate(event) {
-    currentTime = event.seconds;
-    }
-function onVideoPause() {
-    console.log('Час відтворення відео при зупинці:', currentTime);
-    localStorage.setItem('videoplayer-current-time', JSON.stringify(currentTime));
-    }
-function onVideoPlay() {
-    const savedData = localStorage.getItem('videoplayer-current-time');
-    const parseData = JSON.parse(savedData);
-        if (parseData) {
-        currentTime = parseData;
-        player.setCurrentTime(currentTime);
-        console.log('Час відтворення', currentTime);
-        }
-    }
-});
+let savedData = localStorage.getItem('videoplayer-current-time')
+const parseData = JSON.parse(savedData)
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+
+player.on('timeupdate', throttle(onLocalRecordTime, 1000))
+
+function onLocalRecordTime(event) {
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(event))
+};
+
+player.setCurrentTime(parseData ? parseData.seconds : 0)
